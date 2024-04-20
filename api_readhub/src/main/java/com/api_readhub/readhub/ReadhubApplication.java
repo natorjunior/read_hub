5afa622004dfb8d9c8c2ffd58ba8a4b59d7b7d25
eddev7 <eduardo.nog7@gmail.com>
2024-04-20 11:58:05 -0300

    Criação do projeto da API em Spring boot

---
+++ api_readhub/src/main/java/com/api_readhub/readhub/ReadhubApplication.java
@@ -0,0 +1,13 @@
+package com.api_readhub.readhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadhubApplication.class, args);
+	}
+
+}
---
+++ api_readhub/src/test/java/com/api_readhub/readhub/ReadhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_readhub.readhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
